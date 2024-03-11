312fec42ef75efe25891d08eaad1adaed326b28e
herabakirova <herabakirova@gmail.com>
2024-03-11 19:47:24 +0000

    changed

--- helm.sh
+++ helm.sh
@@ -6,6 +6,13 @@ region="us-central1"
 
 gcloud container clusters get-credentials $cluster_name --region $region --project $project_name
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
+kubectl patch svc kubernetes-dashboard --type='json' -p '[{"op":"replace","path":"/spec/type","value":"NodePort"}]' -n kubernetes-dashboard
+k apply -f sa.yaml 
+k apply -f secret.yaml
+apt-get install jq -y
 kubectl proxy
 
 
+kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --serviceaccount=kubernetes-dashboard:admin
+
+kubectl get secret $(kubectl get sa kubernetes-dashboard -o jsonpath='{.secrets[0].name}') -o go-template='{{.data.token | base64decode}}'
