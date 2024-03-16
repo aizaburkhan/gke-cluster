5e033c38d45741abb1ef48ee0d9c40ebc8cef917
herabakirova <153569566+herabakirova@users.noreply.github.com>
2024-03-16 11:40:31 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -13,9 +13,9 @@ resource "google_compute_network" "vpc_network" {
   name                    = var.vpc_name
   project                 = var.project_name 
   routing_mode            = "REGIONAL"
-  auto_create_subnetworks = false
+  auto_create_subnetworks = false  #when set to "false" the network is created in custom subnet mode"
   mtu                     = 1460
-  delete_default_routes_on_create = false
+  delete_default_routes_on_create = false  #when set to "true" it will delete the default route to the Internet
 
   depends_on = [
     google_project_service.compute_api,
